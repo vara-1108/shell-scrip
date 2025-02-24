9ea82232c437655d5a31ae1aeb731eb16afb83a3
vara1108 <varaprasanna0811@gmail.com>
2025-02-24 16:26:10 +0530

    shell script

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -6,6 +6,6 @@ echo "Script name: $0"
 echo "Present working directory: $PWD"
 echo "Home directory of current user: $HOME"
 echo "Which user is running this script: $USER"
-eccho "Process id of current script: $$"
+echo "Process id of current script: $$"
 sleep 60 &
-eccho "Process id of last command in background: $!"
+echo "Process id of last command in background: $!"
