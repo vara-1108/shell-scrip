d196e1887e1e57842be41bba363868ba4db81fea
vara1108 <varaprasanna0811@gmail.com>
2025-03-26 17:32:16 +0530

    expense-shell

--- 18-backup.sh
+++ 18-backup.sh
@@ -53,4 +53,9 @@ echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 
-echo "Files are: $FILES"
+if [ -n $FILES ]
+then
+    echo "Files are: $FILES"
+else
+    echo "No files found older than $DAYS"
+fi
