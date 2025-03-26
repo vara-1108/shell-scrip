b4662f18d5d4116de20d056ee309edf886b40b6e
vara1108 <varaprasanna0811@gmail.com>
2025-03-26 17:26:59 +0530

    expense-shell

--- 18-backup.sh
+++ 18-backup.sh
@@ -51,6 +51,6 @@ fi
 
 echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
-FILES=$(find $SOURCE_DIR -name "*.log" +mtime $DAYS)
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 
 echo "Files are: $FILES"
